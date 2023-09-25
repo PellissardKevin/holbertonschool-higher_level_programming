3f2434b72c4db7e3bb681f96da2a534b72cbcbd3
Kevin.P <kevin.pellissard@gmail.com>
2023-09-25 15:56:33 +0200

    print number until 99 with ,

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number == 99:
+        print("{:02}".format(int(number)))
+    else:
+        print("{:02}, ".format(int(number)), end="")
+
+
