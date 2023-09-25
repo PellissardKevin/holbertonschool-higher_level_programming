b779c0a147caa2623d85bbbbc85fa82a6065c405
Kevin.P <kevin.pellissard@gmail.com>
2023-09-25 13:55:56 +0200

    Remove last_num variable

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_num = int(repr(number)[-1])
+print(f"Last digit of {number} ", end='')
 
-if number <= 0:
-    last_num = -last_num
+number = int(repr(number)[-1])
 
-if last_num > 5:
-    print(f"Last digit of {number} is {last_num} and is greater than 5")
-elif last_num < 6 and last_num != 0:
-    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
-elif last_num == 0:
-    print(f"Last digit of {number} is {last_num} and is 0")
+if number > 5:
+     print(f"is {number} and is greater than 5")
+elif number < 6 and number != 0:
+    print(f"is {number} and is less than 6 and not 0")
+elif number == 0:
+    print(f"is {number} and is 0")
