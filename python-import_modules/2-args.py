0d1ba4fe8c232e7f299b8fad92fcb8db8996838e
Kevin.P <kevin.pellissard@gmail.com>
2023-09-26 15:54:01 +0200

    change codestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,9 @@ import sys
 argv = sys.argv
 
 if __name__ == "__main__":
-    if len(argv) == 1:
+    if len(argv) <= 1:
         print("{} arguments.".format(len(argv) - 1))
     else:
         print("{} arguments:".format(len(argv) - 1))
-        for i in range (1, len(argv)):
+        for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
