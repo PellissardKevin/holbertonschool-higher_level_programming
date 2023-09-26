5820846da4e7aaff8d4d7052a298be071fbd8ed5
Kevin.P <kevin.pellissard@gmail.com>
2023-09-26 16:01:31 +0200

    Update for 1 args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
-
+argv_len = len(argv)
 if __name__ == "__main__":
-    if len(argv) <= 1:
-        print("{} arguments.".format(len(argv) - 1))
+    if argv_len <= 1:
+        print("{} arguments.".format(argv_len - 1))
+    elif argv_len == 2:
+        print("{} argument:".format(argv_len - 1))
+        for i in range(1, argv_len):
+            print("{}: {}".format(i, argv[i]))
     else:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
+        print("{} arguments:".format(argv_len - 1))
+        for i in range(1, argv_len):
             print("{}: {}".format(i, argv[i]))
