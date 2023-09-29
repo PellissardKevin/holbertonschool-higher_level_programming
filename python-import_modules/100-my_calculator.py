a114104ef0bf804e10e1ed6a0f653810936fc19b
Kevin.P <kevin.pellissard@gmail.com>
2023-09-29 10:42:00 +0200

    Fix easy print and change calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from sys import argv, exit
 
 if __name__ == "__main__":
     if len(argv) != 4:
-        print("Unknown operator. Available operators: +, -, * and /")
+        print("./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
