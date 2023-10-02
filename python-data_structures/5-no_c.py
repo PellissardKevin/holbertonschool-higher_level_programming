4336285553e58735226d7c663460e40bd35b88ec
Kevin.P <kevin.pellissard@gmail.com>
2023-10-02 14:55:13 +0200

    add matrix task 6 project 1

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string = list(my_string)
-    for i in my_string:
-        if i == 'c':
-            my_string.remove('c')
-        if i == 'C':
-            my_string.remove('C')
-    my_string = ''.join(my_string)
-    return my_string
+    return ''.join(char for char in my_string if char not in 'cC')
