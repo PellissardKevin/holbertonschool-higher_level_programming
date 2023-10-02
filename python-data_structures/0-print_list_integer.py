1aa045d640b34d4f9cec5555711416839f85a635
Kevin.P <kevin.pellissard@gmail.com>
2023-10-02 10:16:44 +0200

    add if main for import

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range (1, (len(my_list) + 1)):
-        print("{}".format(i))
+
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for i in range (1, (len(my_list) + 1)):
+            print("{}".format(int(i)))
