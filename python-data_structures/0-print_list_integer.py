8d9096c65f4c8c9731d6f455422ea850eca3b719
Kevin.P <kevin.pellissard@gmail.com>
2023-10-02 10:19:10 +0200

    change pycodestyle

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for i in range (1, (len(my_list) + 1)):
-            print("{}".format(int(i)))
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list) + 1):
+        print("{}".format(int(i)))
