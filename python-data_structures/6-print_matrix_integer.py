37ab82beee734f775c4dd244afadf5bfb00836b3
Kevin.P <kevin.pellissard@gmail.com>
2023-10-02 15:43:50 +0200

    Change condition and print value

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(' '.join(map(str, i)))
+    for row in range(len(matrix)):
+        for val in range(len(matrix[row])):
+            print("{}".format(matrix[row][val]), end="")
+            if val != len(matrix[0]) - 1:
+                print(" ", end="")
+        print()
