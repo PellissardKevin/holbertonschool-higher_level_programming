0927a58c4beb378f81226139f119c64f0aa6d80a
Kevin.P <kevin.pellissard@gmail.com>
2023-10-02 15:44:57 +0200

    update print value with :d

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for val in range(len(matrix[row])):
-            print("{}".format(matrix[row][val]), end="")
+            print("{:d}".format(matrix[row][val]), end="")
             if val != len(matrix[0]) - 1:
                 print(" ", end="")
-        print()
+        print("")
