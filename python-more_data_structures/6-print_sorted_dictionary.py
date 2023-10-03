9f67430c51d06767c4c17c4033641360dcd1ebb0
Kevin.P <kevin.pellissard@gmail.com>
2023-10-03 15:35:40 +0200

    Update chmod task 6-12 and change return task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary):
-        print(k, a_dictionary[i], sep=': ')
+        print(i, a_dictionary[i], sep=': ')
