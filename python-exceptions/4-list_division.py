b6cb6450e03982705904da21aa4c146d1a2ad8cc
Kevin.P <kevin.pellissard@gmail.com>
2023-10-05 16:06:25 +0200

    Add task 4-5-6 and advanced task 100 and 101

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    quotient = []
+    for i in range(0, list_length):
+        try:
+            quotient = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            quotient = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            quotient = 0
+        except IndexError:
+            print("out of range")
