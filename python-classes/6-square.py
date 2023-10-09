55e71f9ee78da2c7cdbf521d5bb38d3bf8079d34
Kevin.P <kevin.pellissard@gmail.com>
2023-10-09 19:06:34 +0200

    Remove loop, change format print, update print and change condition position and init

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,13 +8,12 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         if not type(size) is int:
             raise TypeError("size must be an integer")
-        elif int(size) < 0:
+        if int(size) < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        if not type(position) is tuple and len(position) != 2 and\
-        not type(position[0]) is int or not type(position[1]) is int and\
-            position[0] < 0 or position[1] < 0:
+        self.__size = size
+        if (not type(position) is tuple and len(position) != 2) and\
+            (not type(position[0]) is int or not type(position[1]) is int) and\
+                (position[0] < 0 or position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
@@ -37,9 +36,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not type(value) is tuple and len(value) != 2 and\
-        not type(value[0]) is int or not type(value[1]) is int and\
-            value[0] < 0 or value[1] < 0:
+        if (not type(value) is tuple and len(value) != 2) and\
+            (not type(value[0]) is int or not type(value[1]) is int) and\
+                (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
@@ -51,8 +50,6 @@ class Square:
         if self.__size == 0:
             print()
             return
-        if self.__position[1] > 0:
-            print("\n" * self.__position[1], end="")
-        for i in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        print("{}".format("\n" * self.__position[1]), end="")
+        print("{}".format("\n".join(
+            [" " * self.__position[0] + "#" * self.__size] * self.__size)))
