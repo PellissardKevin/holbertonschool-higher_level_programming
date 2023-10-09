bb4d4b1a9013d60d3746eb7ce8ffab08563830ee
Kevin.P <kevin.pellissard@gmail.com>
2023-10-09 19:12:41 +0200

    Change condition position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -11,12 +11,12 @@ class Square:
         if int(size) < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        if (not type(position) is tuple and len(position) != 2) and\
-            (not type(position[0]) is int or not type(position[1]) is int) and\
-                (position[0] < 0 or position[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
+        if type(position) is tuple and len(position) == 2 and\
+            type(position[0]) is int and type(position[1]) is int and\
+                position[0] >= 0 and position[1] >= 0:
             self.__position = position
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
     def size(self):
@@ -36,17 +36,18 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not type(value) is tuple and len(value) != 2) and\
-            (not type(value[0]) is int or not type(value[1]) is int) and\
-                (value[0] < 0 or value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
+        if type(value) is tuple and len(value) == 2 and\
+            type(value[0]) is int and type(value[1]) is int and\
+                value[0] >= 0 and value[1] >= 0:
             self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         return self.__size * self.__size
 
     def my_print(self):
+        """Prints a square with '#'"""
         if self.__size == 0:
             print()
             return
