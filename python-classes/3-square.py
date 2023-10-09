20850593fd138edfeb85fb6143654cd3eee5f2da
Kevin.P <kevin.pellissard@gmail.com>
2023-10-09 11:25:43 +0200

    Add task 3, print area

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains a class definition of 'Square'"""
+
+
+class Square:
+    """Definition of size in square"""
+
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
