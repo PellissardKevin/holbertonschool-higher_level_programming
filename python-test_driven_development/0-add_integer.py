5f340faef3d30ac439df012e10bc29920c8d87d6
Kevin.P <kevin.pellissard@gmail.com>
2023-10-10 14:18:51 +0200

    Update task 0, test without elif, add task 2 matrix divided

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,12 +8,10 @@
 
 def add_integer(a, b=98):
     """Computes the sum of two integers
-        return error if a or b is not interger
+        return error if a or b is not integer
     """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
-    elif not (isinstance(b, int) or isinstance(b, float)):
+    if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
+    return int(a) + int(b)
