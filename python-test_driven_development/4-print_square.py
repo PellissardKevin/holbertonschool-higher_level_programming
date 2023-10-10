3266a7f3f00c30cf059d973dadf6aa1038c85653
Kevin.P <kevin.pellissard@gmail.com>
2023-10-10 19:47:43 +0200

    Add print square unit test task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" prints a square with the character '#'"""
+
+
+def print_square(size):
+    """print a square with a size
+    return an error if size it's not an integer
+    or if size less than 0"""
+    if not type(size) is int:
+        raise TypeError("size must be an integer")
+    if int(size) < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
