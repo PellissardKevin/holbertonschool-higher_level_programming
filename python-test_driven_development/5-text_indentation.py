ce25f6401d5885cdecbcbc0b2424d8175b2f845a
Kevin.P <kevin.pellissard@gmail.com>
2023-10-10 20:18:30 +0200

    Add text indentation.py, and txt for Error task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define text_indentation function"""
+
+def text_indentation(text):
+    """prints a text with 2 new lines after
+    each of these characters: ., ? and :"""
+    if not type(text) is str:
+        raise TypeError("text must be a string")
+
+    special_chr = {'.', ':', '?'}
+    begin = 0
+    for end in range(len(text)):
+        if text[end] in special_chr:
+            line = text[begin:end + 1] + "\n\n"
+            begin = end + 1
+            print("{}".format(line.strip(" ")), end="")
+    if begin <= end:
+        line = text[begin:end + 1]
+        print("{}".format(line.strip(" ")), end="")
