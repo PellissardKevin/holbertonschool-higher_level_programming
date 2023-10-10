db2a1cbbbe018ae926e6a355436f813f825b411a
Kevin.P <kevin.pellissard@gmail.com>
2023-10-10 13:01:30 +0200

    Add comentary for task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+""" Return the addition of two integer
+>>> add_integer(5, 10)
+15
+>>> add_integer(2)
+100"""
+
+
 def add_integer(a, b=98):
-    """Computes the sum of two integers"""
+    """Computes the sum of two integers
+        return error if a or b is not interger
+    """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     elif not (isinstance(b, int) or isinstance(b, float)):
