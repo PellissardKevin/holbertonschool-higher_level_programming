01b9196055cc792f85c111eea9f0606fd51c6cfe
Kevin.P <kevin.pellissard@gmail.com>
2023-10-10 12:13:47 +0200

    Update condition task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """Computes the sum of two integers"""
-    if not (type(a) is int) or (type(a) is float):
+    if (type(a) is int) or (type(a) is float):
+        a = int(a)
+    else:
         raise TypeError("a must be an integer")
-    elif not (type(b) is int) or (type(b) is float):
+    if (type(b) is int) or (type(b) is float):
+        b = int(b)
+    else:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
+
     return a + b
