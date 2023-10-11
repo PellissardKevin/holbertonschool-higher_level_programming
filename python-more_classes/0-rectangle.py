2668711baab942f64a8f1b61662ffaa5869b83cd
Kevin.P <kevin.pellissard@gmail.com>
2023-10-11 14:14:39 +0200

    Add Project 3 task 0, update task 5 project 2

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Contains an empty class definition of 'Rectangle'"""
+
+
+class Rectangle:
+    """Empty definition of 'Rectangle'"""
+    pass
--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,11 +2,13 @@
 """Unittest for max_integer([..])"""
 
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+
+max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
     """Unittests for max_integer()"""
+
     def test_sorted_integers(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
