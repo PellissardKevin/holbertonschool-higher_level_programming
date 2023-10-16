019185d438f1c8e598cd9c641eaf90d9b963c6f1
Kevin.P <kevin.pellissard@gmail.com>
2023-10-16 23:30:17 +0200

    Fix task 1 p2

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Definition of function my_list"""
 
+
 class MyList(list):
     """Class Mylist with list in inherit"""
 
     def print_sorted(self):
         """"Function that prints the list, but sorted """
         print(sorted(self))
-
