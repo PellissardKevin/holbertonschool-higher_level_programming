7620563deaa03df9d2aa2a42b87fbdbfb5a71514
Kevin.P <kevin.pellissard@gmail.com>
2023-10-17 23:41:56 +0200

    Chmod +x all task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,12 +2,11 @@
 """script for adding item in json list"""
 
 
-from sys import argv
+import sys
 import json
-
-
 save_to_json = __import__('5-save_to_json').save_to_json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 if __name__ == "__main__":
     try:
@@ -15,6 +14,6 @@ if __name__ == "__main__":
     except FileNotFoundError:
         json_list = []
 
-    for i in range(len(argv)):
-        json_list.append(argv[i])
+    for i in range(len(sys.argv)):
+        json_list.append(sys.argv[i])
     save_to_json(json_list, "add_item.json")
