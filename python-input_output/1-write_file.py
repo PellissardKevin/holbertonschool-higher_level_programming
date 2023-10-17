8200d8deecafbcfa381a75e53221a1b485b8eef4
Kevin.P <kevin.pellissard@gmail.com>
2023-10-17 23:04:54 +0200

    add com task 1 and 0 project 3

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Definition of write function"""
+
+
 def write_file(filename="", text=""):
     """ function that writes a string to a text file (UTF8)  """
     with open(filename, "w", encoding='utf-8') as file:
