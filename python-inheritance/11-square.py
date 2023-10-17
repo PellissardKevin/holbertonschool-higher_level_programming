718bbbf4c80cc41c076a695a22f36d8481c0c71e
Kevin.P <kevin.pellissard@gmail.com>
2023-10-17 22:32:53 +0200

    update task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""class square """
+""" Class that inherits from BaseGeometry """
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class of a square"""
+    """ class Square that inherits from Rectangle """
+
     def __init__(self, size):
-        self.integer_validator("size", size)
         self.__size = size
+        self.integer_validator("size", size)
         super().__init__(size, size)
 
     def area(self):
         return super().area()
 
     def __str__(self):
-        return "[square] {:d}/{:d}".format(self.__size, self.__size)
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
