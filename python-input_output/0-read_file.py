f9d0ebb01a7043634157a709528e520d752595da
Kevin.P <kevin.pellissard@gmail.com>
2023-10-17 23:03:48 +0200

    add com in task 0 and 1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Definition of read_file function"""
-
-
 def read_file(filename=""):
-    with open(filename, "r", encoding='utf-8') as f:
+    """ function that reads a text file (UTF8) and prints it to stdout """
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Definition of write function"""
-
-
 def write_file(filename="", text=""):
+    """ function that writes a string to a text file (UTF8)  """
     with open(filename, "w", encoding='utf-8') as file:
         return file.write(text)
 
