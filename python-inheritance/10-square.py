cef3bddbf445120a0ed5bb503c78bf107eba07f2
Kevin.P <kevin.pellissard@gmail.com>
2023-10-17 22:23:14 +0200

    added task 10 and 11 project 2, square classes

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class square """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class of a square"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area():
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
