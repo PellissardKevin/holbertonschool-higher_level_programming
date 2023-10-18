36bc34ee34e134e11b659793c1cfa338449e1d96
Kevin.P <kevin.pellissard@gmail.com>
2023-10-18 23:36:56 +0200

    Update task 9 project 2

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
 
     def area(self):
-        return self.__height * self.__width
+        return super().area()
 
     def __str__(self):
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
