ceae4e66c2f83db99665a1feef23d7f9395001f3
Kevin.P <kevin.pellissard@gmail.com>
2023-10-18 23:34:28 +0200

    Pycodestyle task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     list_num = [[1]]
     for i in range(1, n):
         line = [1]
-        for j in range (len(list_num)):
+        for j in range(len(list_num)):
             if j + 1 < len(list_num):
                 line.append(list_num[i - 1][j] + list_num[i - 1][j + 1])
         line.append(1)
