103ca9346dcb7086b7c2db1dd7f13f5633925b66
Kevin.P <kevin.pellissard@gmail.com>
2023-10-24 01:14:17 +0200

    Update gitignore, added load from json, create, from json to string, and save to file method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,43 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON string"""
-        if list_dictionaries is None:
-            return '[]'
-        return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries or [])
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Converting a dict into a json string"""
+        if list_objs:
+            j = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        else:
+            j = '[]'
+        with open(cls.__name__ + '.json', 'w') as f:
+            f.write(j)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """json to string static method"""
+        if json_string:
+            return json.loads(json_string)
+        return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            instance = cls(5, 5)
+        else:
+            instance = cls(5)
+
+        instance.update(**dictionary)
+        return instance
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        try:
+            filename = cls.__name__ + '.json'
+            with open(filename, mode='r') as f:
+                d = cls.from_json_string(f.read())
+            return [cls.create(**x) for x in d]
+        except FileNotFoundError:
+            return []
