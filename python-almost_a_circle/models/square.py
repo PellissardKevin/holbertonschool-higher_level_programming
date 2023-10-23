77410040fb9cb386fc68dee40b9ae68cbd457853
Kevin.P <kevin.pellissard@gmail.com>
2023-10-24 00:02:37 +0200

    Added setter and getter size in square class task 10 p1

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Class Square"""
-
-
 from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
     """Class Square inherit of Rectangle"""
@@ -13,54 +12,20 @@ class Square(Rectangle):
         self.__y = y
 
     @property
-    def width(self):
+    def size(self):
         return self.__width
 
-    @width.setter
-    def width(self, value):
+    @size.setter
+    def size(self, value):
         if not type(value) is int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not type(value) is int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
         self.__height = value
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if not type(value) is int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if not type(value) is int:
-            raise TypeError("y must be an integer")
-        if int(value) < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
 
     def __str__(self):
         """return a strings for print"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
             self.id, self.__x, self.__y, self.__size)
+
