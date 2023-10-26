e1fec79324b0dd6c3e28d209936f85dcf1377235
Kevin.P <kevin.pellissard@gmail.com>
2023-10-26 18:44:00 +0200

    Update to json string for list None and Empty

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,9 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON string"""
-        return json.dumps(list_dictionaries or '[]')
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
