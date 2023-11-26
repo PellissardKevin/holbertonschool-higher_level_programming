d3c7e19bc1b11a574079f957d3b469fe4a5b67b7
Kevin.P <kevin.pellissard@gmail.com>
2023-11-26 13:58:07 +0100

    Update query from task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
         """SELECT cities.id, cities.name, states.name
                 FROM cities
                 INNER JOIN states ON cities.state_id = states.id
-                ORDER BY cities.id ASC"""
-    )
+                ORDER BY cities.id ASC;""")
     rows = cur.fetchall()
 
     for row in rows:
