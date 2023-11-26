32e1415f350af72b133e2dd41292649c63be6ca3
Kevin.P <kevin.pellissard@gmail.com>
2023-11-26 13:55:27 +0100

    Update task 2 add condition to print

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,13 +4,14 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-    )
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name = '{}' ORDER BY id ASC""".format(argv[4]))
+    cur.execute(
+        """SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"""
+        .format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
